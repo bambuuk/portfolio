2a4175f69821aecd1192209f30b6b99e0f780775
Ivan <vanyok.korobka@ukr.net>
2022-01-28 02:24:13 +0200

    added function  showing skills rating in js file

--- dist/js/script.js
+++ dist/js/script.js
@@ -9,3 +9,10 @@ hamburger.addEventListener('click', () => {
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
 });
+
+const counters = document.querySelectorAll('.title__percent');
+const lines = document.querySelectorAll('.skills__scales__line-usage');
+
+counters.forEach((item, i) => {
+    lines[i].style.width = item.innerHTML;
+})
