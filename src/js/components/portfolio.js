e52d4a4517e80e8f81767e5d2e9cc7afbbfd11cc
Ivan <vanyok.korobka@ukr.net>
2023-02-15 17:20:56 +0200

    added info about portfolio

---
+++ src/js/components/portfolio.js
@@ -0,0 +1,48 @@
+function portfolio() {
+  const promoBtns = document.querySelectorAll('.promo__btns');
+  const promoLinks = document.querySelectorAll('.promo__link');
+
+  function setActiceBtn(a) {
+    // let i = [...promoLinks].indexOf(a);
+
+    // console.log(i);
+
+    // portfolioProjects.forEach(a => {
+    //   a.classList.remove("activeProject");
+
+    //   if ([...portfolioProjects].indexOf(a) === i) {
+    //     a.classList.add("activeProject");
+    //   }
+    // })
+
+    // promoBtns.forEach(a => {
+    //   a.classList.remove("activeHeading");
+
+    //   if ([...portfolioHeadings].indexOf(a) === i) {
+    //     a.classList.add("activeHeading");
+    //   }
+    // })
+  }
+
+  // promoBtns.forEach(a => {
+  //   a.addEventListener("mouseover", () => setActiceBtn(a));
+  // })
+  // promoBtns.forEach(a => {
+  //   a.addEventListener("mouseout", () => setActiceBtn(a));
+  // })
+
+  // promoBtns.addEventListener('mouseover', (event) => {
+  //   console.dir(event.target);
+  // });
+
+  // promoBtns.addEventListener('mouseout', (event) => {
+  //   console.log(event.target);
+  // });
+
+
+
+
+
+}
+
+export default portfolio;
--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,7 @@
 import hamburger from "./components/hamburger.js";
+import portfolio from "./components/portfolio.js";
 
 window.addEventListener('DOMContentLoaded', function() {
     hamburger();
+    portfolio();
 });
