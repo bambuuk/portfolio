a58ffac211d1d9c73bd72f6cea810a9ad7273fb8
Ivan <vanyok.korobka@ukr.net>
2023-10-28 22:16:24 +0300

    delete all sections animations

--- src/js/components/hamburger.js
+++ src/js/components/hamburger.js
@@ -2,7 +2,15 @@ function hamburger() {
   const hamburger = document.querySelector('.hamburger'),
     menu = document.querySelector('.menu'),
     closeElem = document.querySelector('.menu__close'),
-    menuOverlay = document.querySelector('.menu__overlay')
+    menuOverlay = document.querySelector('.menu__overlay'),
+    menuItem = document.querySelectorAll('.menu__link');
+
+  menuItem.forEach(item => {
+    item.addEventListener('click', () => {
+      menu.classList.remove('active');
+      document.body.style.overflow = "";
+    });
+  });
 
   hamburger.addEventListener('click', () => {
     menu.classList.add('active');
